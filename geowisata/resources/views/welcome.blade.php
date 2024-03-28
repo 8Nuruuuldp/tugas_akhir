0ff8684de65b3600e67d3882ef6c14ddffb837e8
Nurul Dwi Pajriah <nuruldp8@gmail.com>
2024-03-28 12:01:05 +0700

    first

---
+++ geowisata/resources/views/welcome.blade.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bandung Geowisata</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body>
+        
+    <nav class="font-sans flex flex-col text-center content-center sm:flex-row sm:text-left sm:justify-between py-2 px-12 bg-white shadow sm:items-baseline w-full">
+        <div class="mb-2 sm:mb-0 inner">
+
+            <a href="/home" class="text-2xl no-underline text-grey-darkest hover:text-blue-dark font-sans font-bold">Bandung</a><br>
+            <span class="text-xs text-grey-dark">G e o w i s a t a</span>
+
+        </div>
+
+        <div class="sm:mb-0 self-center">
+            <!-- <div class="h-10" style="display: table-cell, vertical-align: middle;"> -->
+            <a href="#" class="text-md no-underline text-black hover:text-blue-dark py-2 px-6">Link1</a>
+            <a href="#" class="text-md no-underline text-grey-darker hover:text-blue-dark py-2 px-6">Link2</a>
+            <!-- <a href="/two" class="text-lg no-underline text-grey-darkest hover:text-blue-dark ml-2">About Us</a> -->
+            <a href="#" class="text-md no-underline text-grey-darker hover:text-blue-dark ml-2 py-2 px-6">Link3</a>
+            <a href="#" type="button" class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Masuk</a>
+            <!-- </div> -->
+        </div>
+    </nav>
+
+    <section class="bg-slate-800 flex justify-center items-center h-screen">
+        <div class="text-center grid py-8 px-12 mx-auto max-w-screen-xl lg:gap-8 xl:gap-0 lg:py-16 ">
+            <div class="place-self-center text-center mr-auto">
+                <h1 class="mb-4 max-w-2xl text-4xl font-extrabold leading-none md:text-5xl xl:text-6xl dark:text-white">BANDUNG</h1>
+                <p class="items-center text-center mb-6 max-w-2xl font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
+                Sajauh ning panginditan Satebih hing lelengkahan Heup dimana diri miara Mulang tetep ka salira Dina teuteup unggal juru Aya carita nu nyentuh kalbu Kagagas kabayang bayang Saban wayah mawa tenang</p>
+                <a href="#" class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-gray-900 rounded-lg border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
+                    Lihat Peta
+                <svg class="ml-2 -mr-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                </a> 
+            </div>               
+        </div>
+    </section>
+
+    <section class="flex justify-center items-center h-screen">
+        <div class="grid py-8 px-12 mx-auto max-w-screen-xl lg:gap-8 xl:gap-0 lg:py-16 ">
+            <div class="mr-auto grid grid-cols-2">
+                <div class="">
+                    <h1 class="mb-4 max-w-2xl text-4xl font-extrabold leading-none md:text-5xl xl:text-6xl dark:text-slate-800">
+                        Tentang Bandung</h1>
+                    <p class="items-center mb-6 max-w-2xl font-light text-slate-700 lg:mb-8 md:text-lg lg:text-xl dark:text-slate-800">
+                    Sajauh ning panginditan Satebih hing lelengkahan Heup dimana diri miara Mulang tetep ka salira Dina teuteup unggal juru Aya carita nu nyentuh kalbu Kagagas kabayang bayang Saban wayah mawa tenang</p> 
+                </div>
+                <div class="">
+
+                </div>
+
+            </div>
+        </div>
+    </section>
+
+</body>
+</html>
