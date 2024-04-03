69403df5a13df7f925bc55795a7e62298a4d4f8b
Nurul Dwi Pajriah <nuruldp8@gmail.com>
2024-04-03 23:08:30 +0700

    Update Landing-Page

--- geowisata/resources/views/welcome.blade.php
+++ geowisata/resources/views/welcome.blade.php
@@ -5,35 +5,46 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bandung Geowisata</title>
     <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
+        integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
+        crossorigin=""/>
+    <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
+        integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo="
+        crossorigin=""></script>
+    
 </head>
 <body>
-        
-    <nav class="font-sans flex flex-col text-center content-center sm:flex-row sm:text-left sm:justify-between py-2 px-12 bg-white shadow sm:items-baseline w-full">
-        <div class="mb-2 sm:mb-0 inner">
 
-            <a href="/home" class="text-2xl no-underline text-grey-darkest hover:text-blue-dark font-sans font-bold">Bandung</a><br>
-            <span class="text-xs text-grey-dark">G e o w i s a t a</span>
+    <nav class="flex flex-wrap items-center justify-between px-12 py-4 bg-transparent">
+    <img src="#" alt="Bandung Geowisata" width="120" />
 
-        </div>
+    <div class="flex md:hidden">
+        <button id="hamburger">
+            <img class="toggle block" 
+                src="https://img.icons8.com/fluent-systems-regular/2x/menu-squared-2.png" width="40" height="40" />
+            <img class="toggle hidden" src="https://img.icons8.com/fluent-systems-regular/2x/close-window.png" width="40" height="40" />
+        </button>
+    </div>
 
-        <div class="sm:mb-0 self-center">
-            <!-- <div class="h-10" style="display: table-cell, vertical-align: middle;"> -->
-            <a href="#" class="text-md no-underline text-black hover:text-blue-dark py-2 px-6">Link1</a>
-            <a href="#" class="text-md no-underline text-grey-darker hover:text-blue-dark py-2 px-6">Link2</a>
-            <!-- <a href="/two" class="text-lg no-underline text-grey-darkest hover:text-blue-dark ml-2">About Us</a> -->
-            <a href="#" class="text-md no-underline text-grey-darker hover:text-blue-dark ml-2 py-2 px-6">Link3</a>
-            <a href="#" type="button" class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Masuk</a>
-            <!-- </div> -->
-        </div>
+    <div class="toggle hidden md:flex w-full md:w-auto text-right text-bold mt-5 md:mt-0 border-t-2 border-blue-900 md:border-none">        
+        <a href="#" class="block md:inline-block text-blue-900 hover:text-blue-500 px-3 py-3 border-b-2 border-blue-900 md:border-none">Beranda</a>
+        <a href="#" class="block md:inline-block text-blue-900 hover:text-blue-500 px-3 py-3 border-b-2 border-blue-900 md:border-none">Tentang</a>
+        <a href="#" class="block md:inline-block text-blue-900 hover:text-blue-500 px-3 py-3 border-b-2 border-blue-900 md:border-none">Rekomendasi</a>
+        <a href="#" class="block md:inline-block text-blue-900 hover:text-blue-500 px-3 py-3 border-b-2 border-blue-900 md:border-none">Peta Wisata</a>
+    </div>
+
+    <a href="#" class="toggle hidden md:flex w-full md:w-auto px-4 py-2 text-right bg-green-900 hover:bg-green-500 text-white md:rounded">Masuk</a>
+
     </nav>
 
-    <section class="bg-slate-800 flex justify-center items-center h-screen">
+    <section class="bg-[url('https://images.unsplash.com/photo-1549473889-14f410d83298?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')] flex justify-center items-center h-screen">
         <div class="text-center grid py-8 px-12 mx-auto max-w-screen-xl lg:gap-8 xl:gap-0 lg:py-16 ">
             <div class="place-self-center text-center mr-auto">
                 <h1 class="mb-4 max-w-2xl text-4xl font-extrabold leading-none md:text-5xl xl:text-6xl dark:text-white">BANDUNG</h1>
-                <p class="items-center text-center mb-6 max-w-2xl font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
+                <h4 class="mb-4 max-w-2xl text-3xl font-extrabold leading-none md:text-3xl xl:text-4xl dark:text-white">KOTA SEJUTA PESONA</h4>
+                <p class="items-center text-center mb-6 max-w-2xl font-light text-white lg:mb-8 md:text-lg lg:text-xl dark:text-white">
                 Sajauh ning panginditan Satebih hing lelengkahan Heup dimana diri miara Mulang tetep ka salira Dina teuteup unggal juru Aya carita nu nyentuh kalbu Kagagas kabayang bayang Saban wayah mawa tenang</p>
-                <a href="#" class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-gray-900 rounded-lg border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
+                <a href="#" class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-gray-900 rounded-lg border border-gray-300 hover:bg-green-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-white dark:hover:bg-green-700 dark:focus:ring-green-800">
                     Lihat Peta
                 <svg class="ml-2 -mr-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
                 </a> 
@@ -41,22 +52,139 @@
         </div>
     </section>
 
-    <section class="flex justify-center items-center h-screen">
+    <section class="bg-[url('https://images.unsplash.com/photo-1613057263089-7a8c82f2825d?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')] flex justify-center items-center h-screen object-cover w-full object-center">
         <div class="grid py-8 px-12 mx-auto max-w-screen-xl lg:gap-8 xl:gap-0 lg:py-16 ">
             <div class="mr-auto grid grid-cols-2">
                 <div class="">
                     <h1 class="mb-4 max-w-2xl text-4xl font-extrabold leading-none md:text-5xl xl:text-6xl dark:text-slate-800">
-                        Tentang Bandung</h1>
+                        Tentang Kota Bandung</h1>
                     <p class="items-center mb-6 max-w-2xl font-light text-slate-700 lg:mb-8 md:text-lg lg:text-xl dark:text-slate-800">
                     Sajauh ning panginditan Satebih hing lelengkahan Heup dimana diri miara Mulang tetep ka salira Dina teuteup unggal juru Aya carita nu nyentuh kalbu Kagagas kabayang bayang Saban wayah mawa tenang</p> 
                 </div>
                 <div class="">
 
                 </div>
-
             </div>
         </div>
     </section>
+
+    <section>
+        <div class="max-w-screen-xl mx-auto sm:p-10 md:p-16  bg-slate-100">
+            <h1 class="text-center mb-8 text-2xl font-extrabold leading-none md:text-4xl xl:text-5xl dark:text-slate-800">
+                Rekomendasi
+            </h1>
+            <p class="text-center pb-2">
+            Sajauh ning panginditan Satebih hing lelengkahan Heup dimana diri miara Mulang tetep ka salira Dina teuteup unggal juru Aya carita nu nyentuh kalbu Kagagas kabayang bayang Saban wayah mawa tenang</p> 
+            </p>
+            <div class="grid grid-cols-1 md:grid-cols-3 sm:grid-cols-2 gap-8 pt-8">
+
+                <div class="border-r border-b border-l border-gray-400 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r flex flex-col justify-between leading-normal">
+                <img src="https://images.unsplash.com/photo-1683471619890-fb7e51c3201c?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" class="w-500">
+                    <div class="p-4 pt-2">
+                        <div class="mb-8">
+
+                            <h6 class="text-gray-900 font-bold text-lg mb-2">
+                                Gedung Sate
+                            </h6>
+                            <span>
+                                Alamat
+                            </span>
+                            <p class="text-gray-700 text-sm">Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                            Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="border-r border-b border-l border-gray-400 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r flex flex-col justify-between leading-normal">
+                <img src="https://images.unsplash.com/photo-1683471619890-fb7e51c3201c?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" class="w-500">
+                    <div class="p-4 pt-2">
+                        <div class="mb-8">
+
+                            <h6 class="text-gray-900 font-bold text-lg mb-2">
+                                Gedung Sate
+                            </h6>
+                            <span>
+                                Alamat
+                            </span>
+                            <p class="text-gray-700 text-sm">Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                            Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="border-r border-b border-l border-gray-400 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r flex flex-col justify-between leading-normal">
+                <img src="https://images.unsplash.com/photo-1683471619890-fb7e51c3201c?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" class="w-500">
+                    <div class="p-4 pt-2">
+                        <div class="mb-8">
+
+                            <h6 class="text-gray-900 font-bold text-lg mb-2">
+                                Gedung Sate
+                            </h6>
+                            <span>
+                                Alamat
+                            </span>
+                            <p class="text-gray-700 text-sm">Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                            Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.</p>
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+    </section>
+
+    <section>
+
+    <div class="max-w-screen-xl mx-auto sm:p-8 md:p-16">
+        <h4 class="mb-8 font-extrabold leading-none md:text-3xl xl:text-4xl dark:text-slate-800">
+            Peta Wisata
+        </h4>
+
+        <form class="flex flex-col md:flex-row gap-3">
+            <div class="flex gap-2">
+
+                <input type="text" placeholder="Nama Tempat wisata"
+                    class="w-full md:w-80 px-3 h-10 rounded-l border-2 border-slate-300 focus:outline-none focus:border-sky-500">
+
+                <input type="text" placeholder="Kategori"
+                    class="w-full md:w-80 px-3 h-10 rounded-l border-2 border-slate-300 focus:outline-none focus:border-sky-500">
+                    
+                <button type="submit" class="bg-green-500 text-white rounded-r px-2 md:px-3 py-0 md:py-1">Search</button>
+            </div>
+        </form>
+
+        <hr class="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700">
+
+        <div id="map">
+
+        </div>
+
+    </div>
+
+    </section>
+
+    <script>
+        document.getElementById("hamburger").onclick = function toggleMenu() {
+        const navToggle = document.getElementsByClassName("toggle");
+        for (let i = 0; i < navToggle.length; i++) {
+            navToggle.item(i).classList.toggle("hidden");
+        }
+        };
+
+        var map = L.map('map').setView([-6.914744, 107.609810], 13);
+
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 19,
+            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        }).addTo(map);
+
+        var marker = L.marker([-6.9024715, 107.6161269]).addTo(map);
+    </script>
+
+    <style>
+        #map { 
+            height: 100vh;
+            width: 100%; 
+        }
+    </style>
 
 </body>
 </html>
