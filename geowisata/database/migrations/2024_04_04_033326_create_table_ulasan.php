8cd1c33c3e84cdc6cd94239770c02412ab11b671
Nurul Dwi Pajriah <nuruldp8@gmail.com>
2024-04-04 11:23:33 +0700

    migrate

--- geowisata/database/migrations/2024_04_04_033326_create_table_ulasan.php
+++ geowisata/database/migrations/2024_04_04_033326_create_table_ulasan.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('table_ulasan', function (Blueprint $table) {
             $table->id();
+    
             $table->timestamps();
         });
     }
