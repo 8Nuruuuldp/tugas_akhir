84f1bd43fb150b8141e3a797002b09bdbc849707
lindahandayani25 <lindahandayanismakp2ciparay@gmail.com>
2024-04-16 14:53:57 +0700

    Menambah link data wisata

--- geowisata/resources/views/admin/wisata/edit.blade.php
+++ geowisata/resources/views/admin/wisata/edit.blade.php
@@ -13,6 +13,7 @@
             <div class="col-sm-6">
                 <ol class="breadcrumb float-sm-right">
                 <li class="breadcrumb-item"><a href="/">Beranda</a></li>
+                <li class="breadcrumb-item"><a href="/wisata">Data Wisata</a></li>
                 <li class="breadcrumb-item active">Edit Data</li>
                 </ol>
             </div>
