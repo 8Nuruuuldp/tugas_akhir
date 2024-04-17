f86fc3509c3a0ccf7af2c3a0e069174cc1cc3515
lindahandayani25 <lindahandayanismakp2ciparay@gmail.com>
2024-04-17 11:39:43 +0700

    edit 4

--- geowisata/resources/views/admin/wisata/create.blade.php
+++ geowisata/resources/views/admin/wisata/create.blade.php
@@ -1,65 +1,6 @@
 @extends('layouts.admin')
 
 @section('content')
-
-<<<<<<< HEAD
-<div class="card card-primary">
-              <div class="card-header">
-                <h3 class="card-title">Tambah Data Wisata</h3>
-              </div>
-              <!-- /.card-header -->
-              <!-- form start -->
-              <form>
-                <div class="card-body">
-                  <div class="form-group">
-                    <label for="kategori">Kategori Wisata</label>
-                    <select id="kategori" class="form-control">
-                        <option value="">Pilih Kategori</option>
-                        <option value="makanan">Wisata Alam</option>
-                        <option value="minuman">Wisata Pendidikan dan Sejarah</option>
-                        <option value="fashion">Wisata Budaya</option>
-                        <option value="fashion">Wisata Rekreatif</option>
-                        <option value="minuman">Wisata Agro</option>
-                        <option value="fashion">Wisata Religi</option>
-                        <option value="fashion">Kampung Wisata Kreatif</option>
-                    </select>
-                  </div>
-                  <div class="form-group">
-                    <label for="nama_tempat">Nama Tempat Wisata</label>
-                    <input type="text" class="form-control" id="nama_tempat" placeholder="Masukan nama tempat wisata">
-                  </div>
-                  <div class="form-group">
-                    <label for="alamat">Alamat</label>
-                    <input type="text" class="form-control" id="alamat" placeholder="Masukan alamat tempat wisata">
-                  </div>
-                  <div class="form-group">
-                    <label for="deskripsi">Deskripsi</label>
-                    <input type="text" class="form-control" id="deskripsi" placeholder="Masukan deskripsi tempat wisata">
-                  </div>
-                  <div class="form-group">
-                    <label for="gambar">Foto atau Gambar Tempat Wisata</label>
-                    <div class="input-group">
-                      <div class="custom-file">
-                        <input type="file" class="custom-file-input" id="gambar">
-                        <label class="custom-file-label" for="gambar">Pilih berkas</label>
-                      </div>
-                      <div class="input-group-append">
-                        <span class="input-group-text">Upload</span>
-                      </div>
-                      
-                    </div>
-                      <div class="form-group">
-                    <label for="latitude">Latitude</label>
-                    <input type="text" class="form-control" id="latitude" placeholder="Masukan latitude tempat wisata">
-                  </div>
-                  <div class="form-group">
-                    <label for="longitude">Longitude</label>
-                    <input type="text" class="form-control" id="longitude" placeholder="Masukan longitude tempat wisata">
-                  </div>
-                    </div>
-                  </div>
-                  
-=======
 <!-- Content Wrapper. Contains page content -->
 <div class="content-wrapper">
     <!-- Content Header (Page header) -->
@@ -68,7 +9,6 @@
             <div class="row mb-2">
                 <div class="col-sm-6">
                     <h1>Tambah Wisata</h1>
->>>>>>> 75e1347d1ad15709d57dae73f748ee8210c6c0cf
                 </div>
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
