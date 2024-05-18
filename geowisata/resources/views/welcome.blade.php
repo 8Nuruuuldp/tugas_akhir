c489615d01ec02074ef1050613e66a7e8ccae125
faizlovfa <faizlovfa.id@gmail.com>
2024-05-19 01:01:03 +0700

    Landing Page | change font "Poppins"

--- geowisata/resources/views/welcome.blade.php
+++ geowisata/resources/views/welcome.blade.php
@@ -43,7 +43,7 @@
 </head>
 
 <body>
-    <nav class="sticky top-0 z-7 bg-white">
+    <nav class="sticky top-0 z-5 bg-white">
         <div class="flex flex-wrap items-center justify-between px-12 py-4">
             <img src="./img/logo.svg" alt="Bandung Geowisata" width="120" />
 
@@ -59,33 +59,35 @@
             <div
                 class="toggle hidden md:flex w-full md:w-auto text-right text-bold mt-5 md:mt-0 border-t-2 border-blue-900 md:border-none">
                 <a href="#"
-                    class="block md:inline-block text-blue-900 hover:text-blue-500 px-3 py-3 border-b-2 border-blue-900 md:border-none">Beranda</a>
+                    class="font-[Poppins] block md:inline-block text-blue-900 hover:text-blue-500 px-3 py-3 border-b-2 border-blue-900 md:border-none">Beranda</a>
                 <a href="#"
-                    class="block md:inline-block text-blue-900 hover:text-blue-500 px-3 py-3 border-b-2 border-blue-900 md:border-none">Tentang</a>
+                    class="font-[Poppins] block md:inline-block text-blue-900 hover:text-blue-500 px-3 py-3 border-b-2 border-blue-900 md:border-none">Tentang</a>
                 <a href="#"
-                    class="block md:inline-block text-blue-900 hover:text-blue-500 px-3 py-3 border-b-2 border-blue-900 md:border-none">Rekomendasi</a>
+                    class="font-[Poppins] block md:inline-block text-blue-900 hover:text-blue-500 px-3 py-3 border-b-2 border-blue-900 md:border-none">Rekomendasi</a>
                 <a href="#"
-                    class="block md:inline-block text-blue-900 hover:text-blue-500 px-3 py-3 border-b-2 border-blue-900 md:border-none">Peta
+                    class="font-[Poppins] block md:inline-block text-blue-900 hover:text-blue-500 px-3 py-3 border-b-2 border-blue-900 md:border-none">Peta
                     Wisata</a>
                 <a href="/kontak/create"
-                    class="block md:inline-block text-blue-900 hover:text-blue-500 px-3 py-3 border-b-2 border-blue-900 md:border-none">Kontak</a>
+                    class="font-[Poppins] block md:inline-block text-blue-900 hover:text-blue-500 px-3 py-3 border-b-2 border-blue-900 md:border-none">Kontak</a>
             </div>
     </nav>
     <section
         class="bg-[url('https://images.unsplash.com/photo-1549473889-14f410d83298?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')] flex justify-center items-center h-screen">
         <div class="text-center grid py-8 px-12 mx-auto max-w-screen-xl lg:gap-8 xl:gap-0 lg:py-16 ">
             <div class="place-self-center text-center mr-auto">
-                <h1 class="mb-4 max-w-2xl text-4xl font-extrabold leading-none md:text-5xl xl:text-6xl dark:text-white">
+                <h1
+                    class="font-[Poppins] mb-4 max-w-2xl text-4xl font-extrabold leading-none md:text-5xl xl:text-6xl dark:text-white">
                     BANDUNG</h1>
-                <h4 class="mb-4 max-w-2xl text-3xl font-extrabold leading-none md:text-3xl xl:text-4xl dark:text-white">
+                <h4
+                    class="font-[Poppins] mb-4 max-w-2xl text-3xl font-extrabold leading-none md:text-3xl xl:text-4xl dark:text-white">
                     KOTA SEJUTA PESONA</h4>
                 <p
-                    class="items-center text-center mb-6 max-w-2xl font-light text-white lg:mb-8 md:text-lg lg:text-xl dark:text-white">
+                    class="font-[Poppins] items-center text-center mb-6 max-w-2xl font-light text-white lg:mb-8 md:text-lg lg:text-xl dark:text-white">
                     Ayo jelajahi kota Bandung dan temukan berbagai pesonanya. Kamu dapat menekan tombol dibawah untuk
                     menemukan berbagai objek wisata yang menarik di kota Bandung
                 </p>
                 <a href="/petawisata"
-                    class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center bg-white text-gray-900 rounded-lg border border-gray-300 hover:bg-grey-100 focus:ring-4 focus:ring-gray-100 dark:text-dark dark:border-white dark:hover:bg-grey-700 dark:focus:ring-grey-800">
+                    class="font-[Poppins] inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center bg-white text-gray-900 rounded-lg border border-gray-300 hover:bg-grey-100 focus:ring-4 focus:ring-gray-100 dark:text-dark dark:border-white dark:hover:bg-grey-700 dark:focus:ring-grey-800">
                     Temukan Tempat Wisata
                     <svg class="ml-2 -mr-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
                         xmlns="http://www.w3.org/2000/svg">
@@ -103,11 +105,12 @@
             <div class="">
                 <div class="text-center">
                     <h1
-                        class="mb-4 max-w-2xl text-4xl font-extrabold leading-none md:text-5xl xl:text-6xl dark:text-slate-800">
-                        Tentang Kota Bandung</h1>
+                        class="font-[Poppins] text-center mb-8 text-2xl font-extrabold leading-none md:text-4xl xl:text-5xl">
+                        Tentang Kota Bandung </h1>
                     <p
-                        class="items-center mb-6 max-w-2xl font-light text-slate-700 lg:mb-8 md:text-lg lg:text-xl dark:text-slate-800">
-                        Bandung, kota yang dijuluki "Kota Kembang" dan "Paris Van Java", merupakan ibukota Jawa Barat
+                        class="font-[Poppins] inline-flex items-center mb-6 max-w-2xl font-light text-slate-700 lg:mb-8 md:text-lg lg:text-xl dark:text-slate-800">
+                        Bandung, kota yang dijuluki "Kota Kembang" dan "Paris Van Java", merupakan ibukota Jawa
+                        Barat
                         dan metropolitan terbesar di provinsi tersebut.
                         Dikelilingi pegunungan, Bandung menawarkan suasana sejuk dan nyaman dengan udara pegunungan.
                         Keindahan alamnya yang memukau, budaya Sunda yang kaya,
@@ -115,24 +118,25 @@
                         internasional
                     </p>
                 </div>
-                <div class="flex gap-8 ">
-                    <div class="">
-                        <img src="#" class="size-auto rounded">
-                    </div>
-                    <div class="">
-                        <img src="#" class="size-auto rounded">
-                    </div>
-                    <div class="">
-                        <img src="#" class="size-auto rounded">
-                    </div>
+            </div>
 
+            <div class="flex gap-8 hover:gap-6">
+                <div class="">
+                    <img src="./img/gedungmerdeka.png" class="size-auto rounded">
                 </div>
+                <div class="">
+                    <img src="./img/pasarbaru.png" class="size-auto rounded">
+                </div>
+                <div class="">
+                    <img src="./img/asiaafrika.png" class="size-auto rounded">
+                </div>
+
             </div>
         </div>
     </section>
 
     <section>
-        <div class="max-w-screen-xl mx-auto sm:p-10 md:p-16 bg-green-900">
+        <div class="font-[Poppins] max-w-screen-xl mx-auto sm:p-10 md:p-16 bg-green-900">
             <div class="grid grid-cols-1 md:grid-cols-4 sm:grid-cols-3 gap-8 p-8">
 
                 <div class="max-w-sm bg-white rounded-lg shadow">
@@ -142,7 +146,8 @@
                             alt="" />
                     </a>
                     <div class="p-5">
-                        <h5 class="mb-2  text-slate-800 font-bold tracking-tight text-gray-900">Gedung Sate</h5>
+                        <h5 class="font-[Poppins] mb-2  text-slate-800 font-bold tracking-tight text-gray-900">Gedung
+                            Sate</h5>
                         <div class="flex items-center mb-2">
                             <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-yellow-500" viewBox="0 0 20 20"
                                 fill="currentColor">
@@ -288,10 +293,11 @@
                 </div>
             </div>
 
-            <h1 class="text-center mb-8 text-2xl font-extrabold leading-none md:text-4xl xl:text-5xl text-white">
+            <h1
+                class="font-[Poppins] text-center mb-8 text-2xl font-extrabold leading-none md:text-4xl xl:text-5xl text-white">
                 Rekomendasi Destinasi
             </h1>
-            <p class="text-center pb-2 text-white">
+            <p class="font-[Poppins] text-center pb-2 text-white">
                 Bandung siap menyambut Anda dengan berbagai pesonanya. Jelajahi destinasi wisata favorit Anda dan
                 ciptakan
                 kenangan indah di Kota Kembang. Berikut ekomendasi yang dapat Anda kunjungi.
@@ -303,7 +309,7 @@
     <section>
 
         <div class="max-w-screen-xl mx-auto sm:p-8 md:p-16">
-            <h4 class="mb-8 font-bold leading-none md:text-3xl xl:text-4xl dark:text-slate-800">
+            <h4 class="mb-8 font-[Poppins] font-bold leading-none md:text-3xl xl:text-4xl dark:text-slate-800">
                 Peta Wisata
             </h4>
 
@@ -314,7 +320,7 @@
                         placeholder="Search..." oninput="onTyping(this)" />
                     <label for="kategori">
                         <select id="kategoriSelect"
-                            class="w-full md:w-80 px-3 h-10 rounded border-2 border-slate-300 focus:outline-none focus:border-sky-500"
+                            class="font-[Poppins] w-full md:w-80 px-3 h-10 rounded border-2 border-slate-300 focus:outline-none focus:border-sky-500"
                             onchange="onCategoryChange()">
                             <option value="">Kategori Wisata</option>
                             @foreach ($kategori as $kategoriItem)
@@ -337,7 +343,7 @@
 
     <!-- Start Contact -->
     <section id="kontak" class="contact">
-        <div class="container mx-auto px-4 py-8">
+        <div class="container font-[Poppins] mx-auto px-4 py-8">
 
             <!-- Start Contact-Header -->
             <div class="contact-header">
@@ -375,7 +381,7 @@
 
             <!-- Start Contact-Body -->
             <div class="contact-body">
-                <div class="max-w-lg mx-auto bg-white p-8 rounded-lg shadow-lg">
+                <div class="max-w-lg mx-auto bg-white p-8 font-[Poppins] rounded-lg shadow-lg">
                     <h2 class="text-2xl font-extrabold mb-6 text-green-600">Hubungi Kami</h2>
                     @if (session('success'))
                         <div id="successMessage" class="bg-green-500 text-white p-4 rounded mb-4">
@@ -416,13 +422,12 @@
     <!-- End Contact -->
 
     <section class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8">
-        <footer class="bg-white h-px my-8 bg-gray-200 border-0 dark:bg-gray-700">
+        <footer class="bg-white font-[Poppins] h-px my-8 bg-gray-200 border-0 dark:bg-gray-700">
             <div class="mx-auto w-full max-w-screen-xl">
                 <div class=" md:mb-0 py-12 px-16">
                     <a href="#" class="flex items-center">
-                        <img src="#" class="h-8 me-3" alt="Bandung Geowisata" />
-                        <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-slate-900">Bandung
-                            Geowisata</span>
+                        <img src="./img/logo.svg" class="h-12 me-3" alt="Bandung Geowisata" />
+                        <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-slate-900"></span>
                     </a>
                 </div>
                 <div class="grid grid-cols-2 px-16 py-8 lg:py-8 md:grid-cols-4">
