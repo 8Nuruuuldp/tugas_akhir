ff24c39548fe858070ebcd6417518391f2ea42d4
lindahandayani25 <lindahandayanismakp2ciparay@gmail.com>
2024-05-25 12:28:47 +0700

    Ganti dasbor jadi dashboard

--- geowisata/resources/views/admin/dashboard.blade.php
+++ geowisata/resources/views/admin/dashboard.blade.php
@@ -8,12 +8,12 @@
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-6">
-                    <h3 class="m-0">Dasbor</h3>
+                    <h3 class="m-0">Dasboard</h3>
                 </div><!-- /.col -->
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
                         <li class="breadcrumb-item"><a href="/">Beranda</a></li>
-                        <li class="breadcrumb-item active">Dasbor</li>
+                        <li class="breadcrumb-item active">Dasboard</li>
                     </ol>
                 </div><!-- /.col -->
             </div><!-- /.row -->
