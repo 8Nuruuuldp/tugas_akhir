c5b7aa6a225d2abc30934387ab7919d5be5e788d
lindahandayani25 <lindahandayanismakp2ciparay@gmail.com>
2024-05-25 11:04:25 +0700

    Tambah icon footer yang ke hapus

--- geowisata/resources/views/welcome.blade.php
+++ geowisata/resources/views/welcome.blade.php
@@ -38,6 +38,8 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+
 
 
 </head>
@@ -423,7 +425,7 @@
 
 
     <!-- Footer Section -->
-    <footer class="bg-white font-[Poppins] p-3">
+    <footer class="bg-white font-[Poppins]">
         <div class="container mx-auto w-full max-w-screen-xl px-4 py-8">
             <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
                 <!-- Column 1 -->
